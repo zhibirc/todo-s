a22c67a9cd08aee41a07318256faefd8e6bd3fc5
Yaroslav <y.surilov@infomir.com>
2020-11-19 17:50:05 +0200

    add basic routes to server

---
+++ production/server/config.py
@@ -0,0 +1,10 @@
+"""
+Global application configuration. Can store run-time options, API urls, paths, execution flags and so on.
+"""
+
+import os
+
+
+API_VERSION = 'v1'
+SERVER_ROOT = os.path.dirname(__file__)
+STATIC_ROOT = os.path.join(SERVER_ROOT, '../client')
--- production/server/main.py
+++ production/server/main.py
@@ -2,23 +2,14 @@
 The place where Web Server lives.
 """
 from aiohttp import web
-#from .routes import setup as setup_routes
-
-routes = web.RouteTableDef()
+from .routes import routes
 
 
-# serve static assets
-@routes.get('/')
-async def test(request):
-    return web.Response(text='OK')
-
-
-# process authorization actions
-@routes.post('api/sessions')
-async def authorize(request):
-    return web.Response(text='OK')
-
+# create application instance
 app = web.Application()
-app.add_routes(routes)
 
-web.run_app(app)
+# setup routes for application
+app.router.add_routes(routes)
+
+if __name__ == '__main__':
+    web.run_app(app)
