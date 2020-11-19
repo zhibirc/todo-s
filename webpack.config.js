47ec766c4d6c5f816b82d488707b30d998e9816e
Yaroslav <y.surilov@infomir.com>
2020-11-19 18:22:17 +0200

    add dev and prod modes for webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,20 @@
 
 const path = require('path');
 
+const outputModes = {
+    development: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './development/client/build/')
+    },
+    production:  {
+        filename: 'bundle.min.js',
+        path: path.resolve(__dirname, './production/client/')
+    }
+};
+
 module.exports = {
     entry: './src/js/main.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    },
+    output: outputModes[process.env.MODE],
     module: {
         rules: [
             {
