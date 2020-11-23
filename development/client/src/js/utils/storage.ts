05a8d4bf995317840efa4e1815f39d832a2f89a6
Yaroslav <y.surilov@infomir.com>
2020-11-23 17:36:52 +0200

    migrate to TS in storage, refactor return values from set* props

--- development/client/src/js/utils/storage.ts
+++ development/client/src/js/utils/storage.ts
@@ -4,10 +4,9 @@
  * @module
  */
 
-import config from '../config.ts';
-
+import config from '../config';
 // TODO: there is one interesting alternative -- "PouchDB", research it later
-const localForage = require('localforage');
+import * as localForage from 'localforage';
 
 localForage.config({
     driver: localForage.INDEXEDDB,
@@ -20,31 +19,33 @@ localForage.config({
     description: 'User data storage/database for buffering/runtime or fully offline working mode.'
 });
 
+/*interface Storage {
+
+}*/
+
 const storage = {
-    getUserInfo: async () => localForage.getItem(config.STORAGE_KEY_USER_INFO),
+    getUserInfo: async () : Promise<object> => localForage.getItem(config.STORAGE_KEY_USER_INFO),
 
-    setUserInfo: async value => await localForage.setItem(config.STORAGE_KEY_USER_INFO, value),
+    setUserInfo: async (value: object) : Promise<object> => localForage.setItem(config.STORAGE_KEY_USER_INFO, value),
 
-    getAppData: async () => localForage.getItem(config.STORAGE_KEY_APP_DATA),
+    getAppData: async () : Promise<object> => localForage.getItem(config.STORAGE_KEY_APP_DATA),
 
-    setAppData: async value => {
-        let appData = this.appData;
+    setAppData: async (value: object) : Promise<object> => {
+        let appData;
 
         try {
-            appData = JSON.parse(appData);
+            appData = await storage.getAppData();
         } catch ( exception ) {
             appData = {};
-
-            console.error(exception);
         }
 
-        await localForage.setItem(
+        return localForage.setItem(
             config.STORAGE_KEY_APP_DATA,
             Object.assign(appData, value)
         );
     },
 
-    clear: async () => {
+    clear: async () : Promise<void> => {
         sessionStorage.clear();
         await localForage.clear();
     }
